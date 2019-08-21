2555a7605ed1b64d0d0ac1be977ab334457e86e8
cuixt <new_lover@126.com>
2019-08-21 14:46:03 +0800

    test

--- zber-mgr/src/main/java/ServletInitializer.java
+++ zber-mgr/src/main/java/ServletInitializer.java
@@ -8,6 +8,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class ServletInitializer extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
         return application.sources(ZberMgrApplication.class);
     }
 }
